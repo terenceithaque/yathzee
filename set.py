a53ef65eaefd3fc38eee364083a778d92084e957
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-05-11 16:03:49 +0200

    Added update() for SetContainer

--- set.py
+++ set.py
@@ -27,4 +27,18 @@ class SetContainer:
 
         # Iterate over the values of the content and return their sum
         return sum(self.content.values())
+    
+    def update(self, Set:str, score:int):
+        """Updates the the content of the container using the given set and the associated score value."""
+
+        # Assertions
+        assert (type(Set).__name__ == "str"), "The set must be a valid string."
+        assert (type(score).__name__ == "int"), "The score must be a valid integer."
+
+        assert (Set in self.content.keys()), "Cannot modify an unexisting set into a SetContainer."
+
+
+        # Update the SetContainer
+        self.content[Set] = score
+
 
