dc38db3651880ba35a698612803edd8a59ae37b3
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-05-11 15:15:41 +0200

    Checking for the beginner

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from dice import *
 from turn import *
 
 
-# Game turns for the player and the computer
-player_turn = True
-computer_turn = False
+
 
 
  
@@ -13,5 +11,41 @@ computer_turn = False
 
 def play():
     "Game loop"
+    # The player must press enter to start playing
+    input("Please press enter to start the game: ")
 
+
+    # Dice roll list for the player and the computer
+    player_roll = []
+    computer_roll =  []
+
+    # Determine if the player or the computer play first
+    print("Rolling dice to determine who begins...")
+    computer_roll = dice_roll()
+    player_roll = dice_roll()
+    
+
+    # Get the beginner
+    first = first_turn(player_roll, computer_roll)
+
+    print("Computer roll :", computer_roll)
+    print("Your roll :", player_roll)
+
+    # If the player starts first
+    if first == 1:
+        print(f"You did a roll score of {sum(player_roll)} against {sum(computer_roll)} for the computer. You begin first.")
+        player_turn = True
+        computer_turn = False
+
+    # Or if the computer starts first
+    if first == 2:
+        print(f"You did a roll score of {sum(player_roll)} against {sum(computer_roll)} for the computer. Your computer begin first.")
+        player_turn = False
+        computer_turn = True
+
+
+
+# Start the game
+play()
+        
 
