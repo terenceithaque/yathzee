a6066c6c146640ca5748b11a0fbabbd0159d0f0e
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-05-11 14:40:58 +0200

    Added turn()

--- main.py
+++ main.py
@@ -2,6 +2,23 @@
 from dice import *
 
 
-# Dice list
-dice_list = []
-print(new_dice(dice_list))
+# Game turns for the player and the computer
+player_turn = True
+computer_turn = False
+
+
+def turn():
+    "Gives the turn either to the player or the computer according to the last that played a strike."
+
+    # If the player had the turn on the last strike, then give it to the computer
+    if player_turn:
+        player_turn = False
+        computer_turn = True
+
+    # Or if the computer had the turn on the last strike, then give it to the player
+    elif computer_turn:
+        computer_turn = False
+        player_turn = True    
+
+
+
