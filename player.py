0275c431ef7a95afbeb0030ce4101ee751f454b5
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-06-19 20:25:07 +0200

    Player can reroll all dice

--- player.py
+++ player.py
@@ -41,38 +41,48 @@ class Player:
         "Reroll the dice of the player"
         # Ask which dice values should be keeped
         keep_values = input("Enter the dice values you want to keep, separated by a comma :")
-        while not keep_values.split(","):
+
+        # If the player wants to reroll all dice
+        if keep_values == "all":
+             self.last_dice_roll = dice_roll(5)
+             self.possible_sets = possible_sets(self.last_dice_roll)
+             print("New dice roll :", self.last_dice_roll)
+             print("Possible sets :", self.possible_sets)
+
+        # If the player wants to reroll only some dice
+        else:     
+            while not keep_values.split(","):
                 keep_values = input("Enter the dice values you want to keep, separated by a comma :")
 
-        # The dice values must be 1, 2, 3, 4, 5 or 6
-        while not all([value in ["1", "2", "3", "4", "5", "6"]] for value in keep_values.split(",")) :
+            # The dice values must be 1, 2, 3, 4, 5 or 6
+            while not all([value in ["1", "2", "3", "4", "5", "6"]] for value in keep_values.split(",")) :
                 keep_values = input("Enter the dice values you want to keep, separated by a comma :")
                     
-        keep_values = [int(value) for value in keep_values.split(",")]
+            keep_values = [int(value) for value in keep_values.split(",")]
 
-        # Number of dice to reroll
-        n_dice = 5 - len(keep_values)
+            # Number of dice to reroll
+            n_dice = 5 - len(keep_values)
 
-        print("Number of dice to reroll :", n_dice)
+            print("Number of dice to reroll :", n_dice)
 
-        rerolled_dice = dice_roll(n_dice, keep_values)
+            rerolled_dice = dice_roll(n_dice, keep_values)
                     
-        final_dice = []
+            final_dice = []
 
-        # Add kept dice values to the final dice list
-        for dice_val in self.last_dice_roll:
+            # Add kept dice values to the final dice list
+            for dice_val in self.last_dice_roll:
                 if dice_val in keep_values:
                     final_dice.append(dice_val)
 
-        # Add the new rerolled dice to the final dice list
-        final_dice.extend(rerolled_dice)
+            # Add the new rerolled dice to the final dice list
+            final_dice.extend(rerolled_dice)
 
 
-        self.last_dice_roll = copy(final_dice)
+            self.last_dice_roll = copy(final_dice)
 
-        self.possible_sets = possible_sets(self.last_dice_roll)
-        print("New dice roll :", self.last_dice_roll)
-        print("Possible sets :", self.possible_sets)
+            self.possible_sets = possible_sets(self.last_dice_roll)
+            print("New dice roll :", self.last_dice_roll)
+            print("Possible sets :", self.possible_sets)
 
 
     def ask_ignore(self):
