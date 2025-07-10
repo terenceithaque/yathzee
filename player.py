3b4f073909347d2bee4199a66614fff7e7656062
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-07-10 15:22:38 +0200

    Corrected the first validity check in reroll_dice()

--- player.py
+++ player.py
@@ -64,7 +64,7 @@ class Player:
                 keep_values = input("Enter the dice values you want to keep, separated by a comma :")
 
             # The dice values must be 1, 2, 3, 4, 5 or 6
-            while not all([value in "1", "2", "3", "4", "5", "6"] for value in keep_values.split(",")) :
+            while not all(value in ["1", "2", "3", "4", "5", "6"] for value in keep_values.split(",")) :
                 print("The values must be between 1 and 6 included.")
                 keep_values = input("Enter the dice values you want to keep, separated by a comma :")
                     
